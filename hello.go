6a99ef49feb53cc160816b6503ee29f2afe914e4
ProtMello <eitan.hmv@outlook.com>
2026-07-10 15:35:56 -0500

    Initial repository

---
+++ hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("hello from a manually constructed OCI image")
+
+	data, err := os.ReadFile("/etc/day2-message.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print(string(data))
+}
